d4172aeaf9765e8663e5b6098d77a6760a2d7ad5
Kevin Urbain <k.urbain.personnel@gmail.com>
2026-02-13 11:55:11 +0100

    add frontend homepage

--- views/user/home.php
+++ views/user/home.php
@@ -1,1 +1,10 @@
-<h1>hOME</h1>
+<?php
+if ($_SESSION['user']) {
+    header('Location: index.php?action=login');
+    exit;
+}
+$username = $_SESSION['user']['username'];
+?>
+
+<h1>Bienvenue <?= $username ?></h1>
+<a href="index.php?action=logout">Se déconnecter</a>
